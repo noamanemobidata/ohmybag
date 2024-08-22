941d0f20c34e7c988c8ab9b675dc3380ef28f14a
Noamane <nchallioui@gmail.com>
2024-08-22 19:33:28 +0000

    [fix] try fix audio path

--- ui.R
+++ ui.R
@@ -5,7 +5,7 @@ ui <- add_cookie_handlers(fluidPage(
 
   tags$meta(name="description", content="Author: miskowski85@hotmail.fr"),
   tags$meta(name="title", content="Oh My Bag: the heist of the century"),
-  tags$audio(src = "www/audio/audio.mp3", type = "audio/mp3", autoplay = TRUE,loop=TRUE, controls = NA,style="display:none;"), 
+  tags$audio(src = "audio/audio.mp3", type = "audio/mp3", autoplay = TRUE,loop=TRUE, controls = NA,style="display:none;"), 
   use_font("special-elite", "www/css/special-elite.css"),
   shiny::includeCSS("www/css/custom_style.css"), 
   useConfetti(),

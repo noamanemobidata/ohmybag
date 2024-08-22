979fcef9f8ea25342bcbf092b2104fa22f22f912
Noamane <nchallioui@gmail.com>
2024-08-22 21:01:50 +0000

    [fix] add www to path for audio

--- ui.R
+++ ui.R
@@ -1,12 +1,12 @@
 
 
 ui <- add_cookie_handlers(fluidPage(
-  
+  tags$html(lang="en"),
   tags$head(tags$title('🎒 Oh My Bag - Thief Game')),
   tags$head(tags$meta(name="description", content="Author: miskowski85@hotmail.fr")),
   tags$head(tags$meta(name="title", content="Oh My Bag: the heist of the century")),
   tags$audio( controls =NA, autoPlay=TRUE,  loop=TRUE, style="display:none;", 
-    tags$source( src="audio/audio.mp3",  type="audio/mp3")
+    tags$source( src="www/audio/audio.mp3",  type="audio/mp3")
   ), 
   use_font("special-elite", "www/css/special-elite.css"),
   shiny::includeCSS("www/css/custom_style.css"), 

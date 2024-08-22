01e807c341f6d41f4a7f74476065603abd1a7534
Noamane <nchallioui@gmail.com>
2024-08-22 21:28:00 +0000

    delete audio folder

--- ui.R
+++ ui.R
@@ -6,7 +6,7 @@ ui <- add_cookie_handlers(fluidPage(
   tags$head(tags$meta(name="description", content="Author: miskowski85@hotmail.fr")),
   tags$head(tags$meta(name="title", content="Oh My Bag: the heist of the century")),
   tags$audio( controls =NA, autoPlay=TRUE,  loop=TRUE, style="display:none;", 
-    tags$source( src="www/audio/audio.mp3",  type="audio/mp3")
+    tags$source( src="www/audio.mp3",  type="audio/mp3")
   ), 
   use_font("special-elite", "www/css/special-elite.css"),
   shiny::includeCSS("www/css/custom_style.css"), 

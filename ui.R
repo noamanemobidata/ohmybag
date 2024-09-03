0a393637b1811b791b0d748e6145c8d73a396363
Noamane <nchallioui@gmail.com>
2024-09-03 20:23:56 +0000

    fix www  ressources

--- ui.R
+++ ui.R
@@ -6,14 +6,14 @@ ui <- add_cookie_handlers(fluidPage(
   tags$head(tags$meta(name="description", content="Author: miskowski85@hotmail.fr")),
   tags$head(tags$meta(name="title", content="Oh My Bag: the heist of the century")),
   tags$audio( controls =NA, autoPlay=TRUE,  loop=TRUE, style="display:none;", 
-    tags$source( src="www/audio.mp3",  type="audio/mp3")
+    tags$source( src="audio.mp3",  type="audio/mp3")
   ), 
   use_font("special-elite", "www/css/special-elite.css"),
   shiny::includeCSS("www/css/custom_style.css"), 
   useConfetti(),
   tags$head(
-    tags$script(src = "www/js/cursor_update.js"),
-    tags$script(src = "www/js/bag_selection.js")
+    tags$script(src = "js/cursor_update.js"),
+    tags$script(src = "js/bag_selection.js")
   ),  
   uiOutput('timeleft'), 
   uiOutput("goal"), 
